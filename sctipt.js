10da43ec47caadcea14899df155652e62c2f038c
Isaggouv <142433145+Isaggov@users.noreply.github.com>
2024-06-16 12:53:05 -0300

    zzzzzzzzzz

--- sctipt.js
+++
@@ -1,8 +0,0 @@
-function search(){
-    let input =document.getElementById('pesquisa').value
-    input = input.tolowerCase()
-    let p = document,getElementByClassName('texto1 texto2')
-     
-    for(i = 0;< p.length; i++)
-        if(!p[])
-} 
