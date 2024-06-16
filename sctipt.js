b052b98845df65dd8b1b31d0929a693c0d9613d3
Isaggouv <142433145+Isaggov@users.noreply.github.com>
2024-06-16 11:30:35 -0300

    fffff

---
+++ sctipt.js
@@ -0,0 +1,8 @@
+function search(){
+    let input =document.getElementById('pesquisa').value
+    input = input.tolowerCase()
+    let p = document,getElementByClassName('texto1 texto2')
+     
+    for(i = 0;< p.length; i++)
+        if(!p[])
+} 
